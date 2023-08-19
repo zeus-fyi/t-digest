91ce2bd6629e6623d5dad588297e31f1f5bf9cde
alex <alexg89@vt.edu>
2023-08-18 20:17:12 -0700

    adds ext getters

--- src/lib.rs
+++ src/lib.rs
@@ -61,6 +61,14 @@ impl Centroid {
             weight: OrderedFloat::from(weight),
         }
     }
+    #[inline]
+    pub fn ordered_mean(&self) -> OrderedFloat<f64> {
+        self.mean.clone()
+    }
+    #[inline]
+    pub fn ordered_weight(&self) -> OrderedFloat<f64> {
+        self.weight.clone()
+    }
 
     #[inline]
     pub fn mean(&self) -> f64 {
@@ -156,8 +164,23 @@ impl TDigest {
     }
 
     #[inline]
+    pub fn centroids(&self) -> Vec<Centroid> {
+        self.centroids.clone()
+    }
+
+    #[inline]
+    pub fn ordered_sum(&self) -> OrderedFloat<f64> {
+        ordered_float::OrderedFloat(*self.sum)
+    }
+
+    #[inline]
     pub fn count(&self) -> f64 {
         self.count.into_inner()
+    }
+
+    #[inline]
+    pub fn ordered_count(&self) -> OrderedFloat<f64> {
+        ordered_float::OrderedFloat(*self.count)
     }
 
     #[inline]
@@ -166,8 +189,18 @@ impl TDigest {
     }
 
     #[inline]
+    pub fn ordered_max(&self) -> OrderedFloat<f64> {
+        ordered_float::OrderedFloat(*self.max)
+    }
+
+    #[inline]
     pub fn min(&self) -> f64 {
         self.min.into_inner()
+    }
+
+    #[inline]
+    pub fn ordered_min(&self) -> OrderedFloat<f64> {
+        ordered_float::OrderedFloat(*self.min)
     }
 
     #[inline]
